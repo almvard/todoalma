addd5b7bce00308cf1db41656c8906600ce38abd
Alma Cederblad <almce333@student.liu.se>
2023-03-01 17:04:39 +0100

    pagination fixat samt deletion

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { List } from "./components/List";
 import { IListItem } from "./interface/interfaces";
+import Pagination from "@mui/material/Pagination";
 
 function App() {
   const [data, setData] = useState<IListItem[]>([]);
   const [exception, setException] = useState();
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState(1);
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/todos`)
@@ -16,12 +19,33 @@ function App() {
       });
   }, []);
 
+  useEffect(() => {
+    setPages(Math.ceil(data.length / 10));
+  }, [data]);
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  const calcStartIndex = (): number => {
+    return (page - 1) * 10;
+  };
+  const calcStopIndex = (): number => {
+    return calcStartIndex() + 10;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p></p>
-        {data.length > 0 && <List data={data} setData={setData} />}
-      </header>
+    <div className="App-header">
+      <p>Min fina lista</p>
+      <Pagination count={pages} page={page} onChange={handleChange} />
+      {data.length > 0 && (
+        <List
+          data={data}
+          setData={setData}
+          startIndex={calcStartIndex()}
+          stopIndex={calcStopIndex()}
+        />
+      )}
     </div>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,30 +1,49 @@
 import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
 import { IListItem } from "../interface/interfaces";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import "./List.css";
 
 type Props = {
   data: IListItem[];
   setData: (data: IListItem[]) => void;
+  startIndex: number;
+  stopIndex: number;
 };
 
-export const List = ({ data, setData }: Props) => {
+export const List = ({ data, setData, startIndex, stopIndex }: Props) => {
   const handleClick = (index: number) => {
-    console.log(index);
     const newList = [...data];
     newList[index].completed = !newList[index].completed;
+    setData(newList);
+  };
+
+  const handleDelete = (index: number) => {
+    const newList = [...data];
+    newList.splice(index, 1);
     setData(newList);
   };
 
   return (
     <div>
       <FormGroup>
-        {data.map((item: IListItem, index: number) => (
-          <FormControlLabel
-            key={item.id}
-            control={<Checkbox checked={item.completed} />}
-            label={item.title}
-            onChange={() => handleClick(index)}
-          />
-        ))}
+        {data.map(
+          (item: IListItem, index: number) =>
+            index >= startIndex &&
+            index < stopIndex && (
+              <div className="List">
+                <FormControlLabel
+                  key={item.id}
+                  control={<Checkbox checked={item.completed} />}
+                  label={item.title}
+                  onChange={() => handleClick(index)}
+                />
+                <DeleteOutlineIcon
+                  className="Trashicon"
+                  onClick={() => handleDelete(index)}
+                />
+              </div>
+            )
+        )}
       </FormGroup>
     </div>
   );
